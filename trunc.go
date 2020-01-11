c0d09e284ed7243e17a458a3c159eeb4b17cbd2a
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2020-01-11 10:03:30 +0530

    Formatted the files

--- trunc.go
+++ trunc.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    var num float64
-    fmt.Println("Insert a floating point number.")
-    _, err := fmt.Scan(&num)
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println(int(num))
+	var num float64
+	fmt.Println("Insert a floating point number.")
+	_, err := fmt.Scan(&num)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(int(num))
 }
