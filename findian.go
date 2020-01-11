c183ed92e6ec84caf8b980741327db2a702f2c93
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2020-01-11 10:03:53 +0530

    Finding i, a and n

---
+++ findian.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var myString string
+	fmt.Println("Please input a string:")
+	_, err := fmt.Scan(&myString)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if strings.HasPrefix(myString, "i") && strings.HasSuffix(myString, "n") && strings.Contains(myString, "a") {
+		fmt.Println("Found!")
+	}
+}
