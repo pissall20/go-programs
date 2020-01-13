5fed4ca908b9b0fa162d5dc7292579cd1a676c1b
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2020-01-13 14:53:01 +0530

    Formatting slice.go

--- slice.go
+++ slice.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sort"
 	"strconv"
-	"os"
 	"strings"
 )
-
 
 func main() {
 	slice := make([]int, 0, 3)
@@ -18,10 +17,10 @@ func main() {
 
 		fmt.Println("Enter a number:")
 		fmt.Scan(&numString)
-		
-		numString =  strings.ToLower(numString)
 
-		if numString == "x"{
+		numString = strings.ToLower(numString)
+
+		if numString == "x" {
 			os.Exit(0)
 		}
 
