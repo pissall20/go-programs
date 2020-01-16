71fa8bac091e4ed2fb12cf8234f631693fcde108
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2020-01-16 09:13:42 +0530

    Finished assignment of animals

---
+++ module2/animals.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Animal struct {
+	food, locomotion, noise string
+}
+
+func (a Animal) Eat() {
+	fmt.Println(a.food)
+}
+
+func (a Animal) Move() {
+	fmt.Println(a.locomotion)
+}
+
+func (a Animal) Speak() {
+	fmt.Println(a.noise)
+}
+
+
+func main() {
+	cow := Animal{food:"grass", locomotion:"walk", noise:"moo"}
+	bird := Animal{food: "worms", locomotion: "fly", noise: "peep"}
+	snake := Animal{food: "mice", locomotion: "slither",noise: "hsss"}
+	dict := map[string]Animal{
+		"cow": cow,
+		"bird": bird,
+		"snake": snake,
+	}
+	for {
+		var animal, action string
+		fmt.Print(">")
+		fmt.Scan(&animal)
+		fmt.Print(">")
+		fmt.Scan(&action)
+		if action == "eat" {
+			dict[animal].Eat()
+		} else if action == "move" {
+			dict[animal].Move()
+		} else if action == "speak" {
+			dict[animal].Speak()
+		}
+	}
+}
